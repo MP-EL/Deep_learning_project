a65913d7242055f054dcf287f1ddc7f180393300
MP-EL <62695168+MP-EL@users.noreply.github.com>
2022-01-02 17:41:01 +0100

    Add files via upload

---
+++ vgg16/bceloss/submit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "Setting up BSUB settings..."
+#BSUB -q gpuv100
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -J vgg16-bce
+#BSUB -n 1
+#BSUB -W 24:00
+#BSUB -R "rusage[mem=16GB]"
+#BSUB -o logs/%J.out
+#BSUB -e logs/%J.err
+#BSUB -u s174831@student.dtu.dk
+#BSUB -B 
+#BSUB -N
+
+echo "Running script..."
+
+module load python3/3.9.6
+module load cuda/8.0
+module load cudnn/v7.0-prod-cuda8
+module load ffmpeg/4.2.2
+
+
+python3 vgg16-bceloss.py
